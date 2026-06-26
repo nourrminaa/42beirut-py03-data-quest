64ef14c82c17dd751ccaed47eb2faa07da401ba3
nourrminaa <184600463+nourrminaa@users.noreply.github.com>
2026-06-26 14:34:14 +0300

    fix: flake8

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -3,11 +3,12 @@ import math
 
 def get_player_pos() -> tuple:
     while True:
-        input_val = input("Enter new coordinates as floats in format 'x,y,z': ")
+        input_val = input("Enter new coordinates as floats\
+                           in format 'x,y,z': ")
         parts = input_val.split(',')
         if len(parts) != 3:
             print("Invalid syntax")
-            continue  # returns to while loop on top
+            continue
         valid = True
         coords = []
         for part in parts:
@@ -29,12 +30,20 @@ def main() -> None:
     pos1 = get_player_pos()
     print(f"Got a first tuple: {pos1}")
     print(f"It includes: X={pos1[0]}, Y={pos1[1]}, Z={pos1[2]}")
-    dist_origin = round(math.sqrt(pos1[0]**2 + pos1[1]**2 + pos1[2]**2), 4)
+    dist_origin = round(
+        math.sqrt(pos1[0]**2 + pos1[1]**2 + pos1[2]**2), 4
+    )
     print(f"Distance to center: {dist_origin}")
 
     print("\nGet a second set of coordinates")
     pos2 = get_player_pos()
-    dist = round(math.sqrt((pos2[0]-pos1[0])**2 + (pos2[1]-pos1[1])**2 + (pos2[2]-pos1[2])**2), 4)
+    dist = round(
+        math.sqrt(
+            (pos2[0]-pos1[0])**2 +
+            (pos2[1]-pos1[1])**2 +
+            (pos2[2]-pos1[2])**2
+        ), 4
+    )
     print(f"Distance between the 2 sets of coordinates: {dist}")
 
 
